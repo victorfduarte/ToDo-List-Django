009d58a88fc89574c8afa4b458da75212a5096c6
Paulo Victor <victorfduarte18@gmail.com>
2022-10-21 10:25:00 -0300

    CRUD - Leitura concluída

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
